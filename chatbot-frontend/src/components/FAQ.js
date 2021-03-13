d93ed388061b437b19ee270e2637eb0f9931247d
Sonal Prabhu <sonalsprabhu16@gmail.com>
2021-03-13 23:10:12 +0530

    Created chatbot flow to send context to backend

--- chatbot-frontend/src/components/FAQ.js
+++ chatbot-frontend/src/components/FAQ.js
@@ -3,26 +3,83 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 export default function FAQ(props) {
-  const mapper={
-    "/stocks":"Stocks",
-    "/fd":"FDs",
-    "/gold":"Gold",
-    "/mutualfund":"Mutual Funds"
+  var mapper={
+    "stocks":'Stocks',
+    "fd":'FDs',
+    "gold":'Gold',
+    "mutualfund":'Mutual Funds'
   }
-
     const currentLoc = window.location.pathname;
     const [options,setOptions]=useState([]);
-    const currentRoute = mapper[currentLoc];
+   
 
     useEffect(async () => {
+      const products = ['Stocks','FDs','Gold','Mutual Funds'];
+      var userId;
+      if(props.user == 'guest')
+        userId = null;
+      else
+      userId ="604a5406f9fce44e70ed0f94";  //Sample user Id from database used for time being
+      var context = currentLoc.split('/');
+      var contextLength = context.length;
+      var categoryName;
+      var mainCategory = context[contextLength-1];
+      if(contextLength === 2)
+      {
+        if(products.indexOf(mapper[context[contextLength-1]]) > -1 )
+        {
+          categoryName = mapper[context[contextLength-1]];
+          var questions = await axios.get(`http://localhost:8081/search-on-category`,{ params: { categoryName:categoryName} })
+          .then(res => {
+            return res.data;
+          });
+          setOptions(questions);
 
-      var questions = await axios.get(`http://localhost:8081/search-on-category`,{ params: { categoryName: currentRoute } })
-      .then(res => {
-        return res.data;
-      });
-      setOptions(questions);
+        }
+        else if(mainCategory=='orders' && props.user!='guest')
+        {
+          var questions = await axios.get(`http://localhost:8081/user-specific-order-details`,{ params: { user:userId} })
+          .then(res => {
+            return res.data;
+          });
+          setOptions(questions);
 
-  }, []);
+        }
+        else if(mainCategory=='account' && props.user!='guest')
+        {
+          var questions = await axios.get(`http://localhost:8081/user-account-questions`,{ params: { user:userId} })
+          .then(res => {
+            return res.data;
+          });
+          setOptions(questions);
+        }
+      }
+      else if(contextLength === 3)
+      {
+        var orderId;
+        var productId="604a5406f9fce44e70ed0f97";
+        mainCategory = mapper[context[contextLength-2]]
+        if(products.indexOf(mapper[context[contextLength-2]]) > -1 )
+        {
+          var questions = await axios.get(`http://localhost:8081/product-specific-questions`,{ params: { product:productId,user:userId} })
+          .then(res => {
+            return res.data;
+          });
+          setOptions(questions);
+        }
+        else if(mainCategory === 'orders')
+        {
+          var questions = await axios.get(`http://localhost:8081/order-specific-questions`,{ params: { order: orderId,user:userId} })
+          .then(res => {
+            return res.data;
+          });
+          setOptions(questions);
+        }
+      }
+  
+      
+  }, []); 
+  
 
   
       const optionsMarkup = options.map((option) => (
